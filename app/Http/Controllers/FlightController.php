75f40d7bcf33bac736d80603f965b28396679c2a
a000011 <anton.nadikin@gmail.com>
2021-10-15 18:06:14 +0400

    update FlightController

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -2,32 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ErrorResource;
 use Illuminate\Http\Request;
 use App\Models\Flights;
 use App\Http\Resources\FlightsResource;
 use App\Models\Booking;
+use Illuminate\Support\Facades\Validator;
 
 class FlightController extends Controller
 {
+    private $status;
+    private $response;
+
     public function getFlight(Request $request)
     {
-        $flightsTo = [];
-        $flightsBack = [];
-
-        $bookings = Booking::where('date_from', '>=', $request->date1)
-            ->where('date_back', '<=', $request->date2)
-            ->get();
-
-        foreach ($bookings as $book){
-            $flightsTo[] = new FlightsResource($book->flightTo());
-            $flightsBack[] = new FlightsResource($book->flightBack());
-        }
-
-        return response()->json([
-            'data' => [
-                'flights_to' => $flightsTo,
-                'flights_back' => $flightsBack,
-            ]
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|string',
+            'to' => 'required|string',
+            'date1' => 'required|date',
+            'date2' => 'date',
+            'passengers' => 'required|Numeric',
         ]);
+        $errors = $validator->errors();
+
+        if ($errors->messages()) {
+            $this->status = 422;
+            $this->response = new ErrorResource($errors);
+        }
+        else {
+            $flightsTo = [];
+            $flightsBack = [];
+
+            $bookings = Booking::where('date_from', '>=', $request->date1)
+                ->where('date_back', '<=', $request->date2)
+                ->get();
+
+            foreach ($bookings as $book){
+                $flightsTo[] = new FlightsResource($book->flightTo());
+                $flightsBack[] = new FlightsResource($book->flightBack());
+            }
+
+
+            $this->status = 200;
+            $this->response = [
+                'data' => [
+                    'flights_to' => $flightsTo,
+                    'flights_back' => $flightsBack,
+                ]
+            ];
+        }
+        return response()->json($this->response, $this->status);
     }
 }
